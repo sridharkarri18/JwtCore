8e59542db11b0eaeca501a794134293cd4fe77e8
Sridhar1808 <sridhar.karri@imaginnovate.com>
2024-02-27 11:26:56 +0530

    Updated the Swagger implentation

--- src/main/java/com/example/JwtCore/config/Appconfig.java
+++ src/main/java/com/example/JwtCore/config/Appconfig.java
@@ -2,6 +2,12 @@ package com.example.JwtCore.config;
 
 import com.example.JwtCore.security.JwtAuthenticationEntryPoint;
 import com.example.JwtCore.security.JwtFilter;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -65,5 +71,28 @@ public class Appconfig {
         daoAuthenticationProvider.setPasswordEncoder(passwordEncoder);
         return daoAuthenticationProvider;
     }
+
+    //Swagger
+    private SecurityScheme createAPIKeyScheme() {
+        return new SecurityScheme()                  // Creating a new instance of SecurityScheme
+                .type(SecurityScheme.Type.HTTP)      // Setting the type of security scheme to HTTP
+                .bearerFormat("JWT")                 // Setting the bearer format to "JWT"
+                .scheme("bearer");                   // Setting the scheme to "bearer"
+    }
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .addSecurityItem(new SecurityRequirement().addList("Bearer Authentication"))
+                .components(new Components().addSecuritySchemes("Bearer Authentication", createAPIKeyScheme()))
+                .info(new Info()
+                        .title("Spring Jwt Security")
+                        .version("1.0")
+                        .contact(new Contact()
+                                .name("Sridhar")
+                                .email("sridharkarri18@gmail.com")));
+
+    }
+
 
 }
